3bac17fb9147e5a240fd39bcb16c7dfbad09da63
balabalalalala <2367890694>
2023-10-21 18:37:37 +0800

    添加了李馨怡的代码

---
+++ code/李馨怡/keren.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+typedef struct
+{
+    string o;
+} Oo;
+
+int main()
+{
+    Oo O;
+    O.o = "尊嘟假嘟";
+    cout << O.o << endl;
+    system("pause");
+    return 0;
+}
